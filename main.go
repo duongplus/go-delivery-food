6370fcd9050746ef7d1bd0006046c83499e29155
Duong Dang <duongplus1011@gmail.com>
2022-07-21 00:10:46 +0700

    add gin

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "food_delivery:19e5a718a54a9fe0559dfbce6908@tcp(127.0.0.1:3306)/food_delivery?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := runService(db); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func runService(db *gorm.DB) error {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"message": "pong",
+		})
+	})
+
+	return r.Run(":8080")
 }
